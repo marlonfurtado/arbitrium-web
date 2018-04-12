4668b9642d31703c049aefceacbd526b6d896a92
arthur.pasqualon <arthur.pasqualon@acad.pucrs.br>
2018-04-12 20:54:40 -0300

    Add links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,12 @@
   <div class="project-name m-t-20">
     {{projectName}}
   </div>
-  <a href="http://www.tools.ages.pucrs.br/arbitrium/web">Acesse o repositório</a>
+  <a href="http://www.tools.ages.pucrs.br/arbitrium/web">Acesse o repositório </a>
+   <ul>
+  <li>VueJS:<a target="_blank" href="https://br.vuejs.org/v2/guide/"> guia completo </a></li>
+  <li>Frontend:<a target="_blank" href="http://www.tools.ages.pucrs.br/arbitrium/api/wikis/frontend"> saiba mais sobre nossa aplicação web </a></li>
+  <li>Estrutura do projeto web:<a target="_blank" href="http://vuejs-templates.github.io/webpack/"> vue-cli webpack </a> </li>
+</ul> 
 </div>
 </template>
 
